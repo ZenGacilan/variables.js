31fcd6a21278351ed1f5513d2596d81ce7e39492
ZenGacilan <potapetekau@gmail.com>
2024-09-09 19:50:28 +0800

    initial commit

--- add.js
+++ add.js
@@ -1,9 +1,9 @@
 const name = "gian carlo"; 
-const whatDoYouWannaBecomeInYourLife = "game developer";
+const whatDoYouWannaBecomeInYourLife = "programmer";
 const gender = "male";
 const twitterHandle = "@gacilangian";
 
 console.log("gian carlo:", name);
-console.log("game developer:", whatDoYouWannaBecomeInYourLife);
+console.log("programmer:", whatDoYouWannaBecomeInYourLife);
 console.log("Male:", gender);
 console.log("@gacilangian:", twitterHandle);
